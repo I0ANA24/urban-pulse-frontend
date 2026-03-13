edc0694a287e5922e5c1ece869e1fd208c983b45
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2026-03-13 21:50:43 +0200

    Filters

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,36 @@ import EventCard from "@/components/EventCard";
 import { Event } from "@/types/Event";
 import Image from "next/image";
 
-const FILTERS = ["GENERAL", "EMERGENCY", "SKILL", "LEND"];
+// const FILTERS = ["GENERAL", "EMERGENCY", "SKILL", "LEND"];
+
+type FilterType = {
+  title: string;
+  textColor: string;
+  bgColor: string;
+};
+
+const FILTERS: FilterType[] = [
+  {
+    title: "GENERAL",
+    textColor: "#4D3B03",
+    bgColor: "#FFF081",
+  },
+  {
+    title: "EMERGENCY",
+    textColor: "#FFFFFF",
+    bgColor: "#A53A3A",
+  },
+  {
+    title: "SKILL",
+    textColor: "#04007D",
+    bgColor: "#BEDCF5",
+  },
+  {
+    title: "LEND",
+    textColor: "#023612",
+    bgColor: "#4ADE80",
+  },
+];
 
 export default function DashboardPage() {
   const [events, setEvents] = useState<Event[]>([]);
@@ -61,7 +90,7 @@ export default function DashboardPage() {
   return (
     <div className="w-full pb-[8vh]">
       {/* logo, event, weather and filter buttons */}
-      <div className="w-full py-4 flex flex-col items-center gap-4">
+      <div className="w-full py-2 flex flex-col items-center gap-4 mb-4">
         {/* logo */}
         <div className="flex items-center gap-1">
           <h1 className="font-montagu text-white text-[32px]">UrbanPulse</h1>
@@ -69,7 +98,7 @@ export default function DashboardPage() {
         </div>
 
         {/* event and weather */}
-        <div className="w-full h-28 flex gap-3 items-center px-3 py-4 justify-center relative">
+        <div className="w-full flex gap-3 items-center p-3 py-4 justify-center relative">
           <Image
             src="/rectangle.svg"
             width={360}
@@ -78,11 +107,11 @@ export default function DashboardPage() {
             priority
             className="absolute object-cover z-0 top-0 w-full h-full rounded-3xl"
           />
-          <div className="flex-1 h-full z-2 bg-weather-nice rounded-2xl flex flex-col justify-center p-4">
+          <div className="flex-1 z-2 bg-weather-nice rounded-2xl flex flex-col justify-center p-2 px-4">
             <p className="w-full font-bold text-lg">Event</p>
             <p className="w-full font-light">Game Night</p>
           </div>
-          <div className="flex-1 h-full z-2 bg-weather-nice rounded-2xl flex justify-between items-center p-4">
+          <div className="flex-1 h-full z-2 bg-weather-nice rounded-2xl flex justify-between items-center p-2 px-4">
             <div className="flex flex-col">
               <p className="w-full font-bold text-xl">21° C</p>
               <p className="w-full font-light">Sunny</p>
@@ -104,29 +133,30 @@ export default function DashboardPage() {
           >
             {FILTERS.map((filter) => (
               <button
-                key={filter}
-                onClick={() =>
-                  setActiveFilter(filter === activeFilter ? "ALL" : filter)
-                }
-                className="shrink-0 py-3 rounded-2xl text-xs font-bold bg-[#facc15] text-black snap-start"
-                style={{ width: "calc(33.333% - 6px)" }}
+                key={filter.title}
+                onClick={() => setActiveFilter(filter.title)}
+                className="shrink-0 w-25 h-11 rounded-[10px] text-xs font-bold bg-yellow-primary text-black snap-start"
+                style={{
+                  backgroundColor: filter.bgColor,
+                  color: filter.textColor,
+                }}
               >
-                {filter}
+                {filter.title}
               </button>
             ))}
           </div>
 
-          {/* Fade dreapta */}
-          <div className="absolute right-0 top-0 bottom-0 w-8 bg-linear-to-l from-background to-transparent pointer-events-none" />
+          {/* right fade */}
+          <div className="absolute -right-10 top-0 bottom-0 w-16 bg-linear-to-l from-background to-transparent pointer-events-none" />
         </div>
 
-        {/* Dots indicator */}
+        {/* dots */}
         <div className="flex justify-center gap-1.5 -mt-2">
           <div
-            className={`h-1.5 rounded-full transition-all ${!scrolledRight ? "w-4 bg-white/60" : "w-1.5 bg-white/20"}`}
+            className={`h-1.5 rounded-full transition-all ${!scrolledRight ? "w-1.5 bg-white/60" : "w-1.5 bg-white/20"}`}
           />
           <div
-            className={`h-1.5 rounded-full transition-all ${scrolledRight ? "w-4 bg-white/60" : "w-1.5 bg-white/20"}`}
+            className={`h-1.5 rounded-full transition-all ${scrolledRight ? "w-1.5 bg-white/60" : "w-1.5 bg-white/20"}`}
           />
         </div>
       </div>
